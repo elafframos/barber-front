9d93b6c0ebdcbe3f5b92402a2f5f2d5ac36e1d3c
elafframos <elaff.dev@gmail.com>
2026-06-15 20:04:47 -0300

    Conexão com git

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Barber.</h1>
+        <p>
+          <a href="">Início</a>
+          <a href="">Agendar</a>
+          <a href="">Admin</a>
+        </p>
+
+        <button>Entar</button>
+      </header>
+      <hr />
+      <main>
+        <a href=""></a>
+      </main>
+
+      <footer>
+        
+      </footer>
+    </>
+  )
+}
+
+export default App
