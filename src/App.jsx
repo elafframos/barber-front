cf8aed137fd507035132c3fa5f3452c0fbe5c8aa
elafframos <elaff.dev@gmail.com>
2026-06-16 21:08:23 -0300

    Melhoria no App.jsx, adicionando botão de entrar e estilização da nav

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,36 @@ function App() {
         <button>Entrar</button>
       </header>
       <hr />
-      <main>
-        <a href=""></a>
+      <main> 
+
+        <p className='headline'>
+          <div class="linha-horizontal"></div>  <h3>BARBEARIA PREMIUM</h3> <div class="linha-horizontal"></div>
+        </p >
+
+        <p className='title'>
+          O corte certo, <br />
+          na hora certa.
+        </p>
+
+        <br />
+        <br />
+
+        <p className='sub-title'>
+          Agende online em segundos. Sem filas, sem espera — só você e o melhor acabamento da cidade.
+        </p>
+
+        <br />
+        <br />
+
+        <p className='agend-buttons'>
+          <button>Agendar agora</button>
+
+          <button>Ver meus agendamentos</button>
+        </p>
+
+        <section className='services'>
+          
+        </section>
       </main>
 
       <footer>
